7589d94b6c61c2708c5a80baaf8befb975ca3943
ngotanloi0709 <52100439@student.tdtu.edu.vn>
2023-11-01 16:16:13 +0700

    P1 - Init Spring project

---
+++ src/main/java/com/fighting/phonesellingweb/PhoneSellingWebApplication.java
@@ -0,0 +1,13 @@
+package com.fighting.phonesellingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneSellingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneSellingWebApplication.class, args);
+    }
+
+}
