bebad2e551de6d787be1b6c03f5b6f266735cfb7
NguyenPhuongHaoThien <phuongit9902@gmail.com>
2023-11-27 19:56:00 +0700

    --P14 - Add Homepage & Pageing

--- src/main/java/com/fighting/phonesellingweb/controlller/HomeController.java
+++ src/main/java/com/fighting/phonesellingweb/controlller/HomeController.java
@@ -1,27 +1,52 @@
 package com.fighting.phonesellingweb.controlller;
 
+import com.fighting.phonesellingweb.model.Phone;
 import com.fighting.phonesellingweb.model.User;
+import com.fighting.phonesellingweb.service.BrandService;
+import com.fighting.phonesellingweb.service.PhoneService;
 import com.fighting.phonesellingweb.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
-@AllArgsConstructor
 public class HomeController {
-    private UserService userService;
+    private final PhoneService phoneService;
+    private final BrandService brandService;
+
+    private final UserService userService;
+
+    public HomeController(PhoneService phoneService, BrandService brandService, UserService userService) {
+        this.phoneService = phoneService;
+        this.brandService = brandService;
+        this.userService = userService;
+    }
 
     @GetMapping({"", "/", "/home"})
-    public String getHome(@CookieValue(name="email", required = false) String email, Model model) {
+    public String home(@CookieValue(name="email", required = false) String email,
+                       @RequestParam(defaultValue = "1") int page,
+                       @RequestParam(defaultValue = "3") int size, Model model) {
         if (email != null) {
             User user = userService.findUserByEmail(email);
             model.addAttribute("user", user);
         }
 
+        Page<Phone> randomPhonePage = phoneService.findRandomPhones(page, size);
+        model.addAttribute("randomPhonePage", randomPhonePage);
+        model.addAttribute("brands", brandService.findAllBrands());
+
         return "index";
     }
+
+
 }
--- src/main/java/com/fighting/phonesellingweb/service/PhoneService.java
+++ src/main/java/com/fighting/phonesellingweb/service/PhoneService.java
@@ -3,9 +3,15 @@ package com.fighting.phonesellingweb.service;
 import com.fighting.phonesellingweb.model.Phone;
 import com.fighting.phonesellingweb.repository.PhoneRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -30,5 +36,26 @@ public class PhoneService {
 
     public void deletePhone(Integer id) {
         phoneRepository.deleteById(id);
+    }
+
+
+    // Lấy danh sách sản phẩm ngẫu nhiên
+    public List<Phone> findRandomPhones(int limit) {
+        List<Phone> allPhones = phoneRepository.findAll();
+        Collections.shuffle(allPhones);
+        return allPhones.stream()
+                .filter(phone -> phone.getImageUrl() != null)
+                .limit(limit)
+                .collect(Collectors.toList());
+    }
+    // Lấy sản phẩm theo thương hiệu
+    public List<Phone> findPhonesByBrand(int brandId) {
+        return phoneRepository.findByBrandId(brandId);
+    }
+
+    public Page<Phone> findRandomPhones(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        List<Phone> randomPhones = phoneRepository.findRandomPhones(pageable);
+        return new PageImpl<>(randomPhones, pageable, randomPhones.size());
     }
 }
