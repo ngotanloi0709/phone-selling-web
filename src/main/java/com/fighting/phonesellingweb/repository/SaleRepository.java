39b501c3764ce11411931619fc260effd7c4916b
Nguyễn Minh Phương <116414967+NguyenPhuongHaoThien@users.noreply.github.com>
2023-11-30 21:53:35 +0700

    --P18 - List Product, Sort List Product (defint, new,price) (#12)

--- src/main/java/com/fighting/phonesellingweb/repository/SaleRepository.java
+++ src/main/java/com/fighting/phonesellingweb/repository/SaleRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Integer> {
 
     @Query("SELECT s FROM Sale s ORDER BY s.quantitySold DESC")
     Page<Sale> findBestSellingPhones(Pageable pageable);
+
 }
