f328da6468b8abf11011f53413408c37b48aeb3c
Loi Ngo <ngotanloi0709@gmail.com>
2023-12-04 11:51:39 +0700

    P31 - Add Cart counting on Navbar

--- src/main/java/com/fighting/phonesellingweb/controlller/GlobalControllerAdvice.java
+++ src/main/java/com/fighting/phonesellingweb/controlller/GlobalControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.fighting.phonesellingweb.controlller;
 
+import com.fighting.phonesellingweb.model.CartItem;
 import com.fighting.phonesellingweb.model.User;
+import com.fighting.phonesellingweb.service.CartService;
 import com.fighting.phonesellingweb.service.UserService;
 import com.fighting.phonesellingweb.util.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,18 +14,29 @@ import org.springframework.ui.Model;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 @ControllerAdvice
 @AllArgsConstructor
 public class GlobalControllerAdvice {
-    private final UserService userService;
+    private UserService userService;
+    private CartService cartService;
 
     @ModelAttribute
     public void addUserAttributes(@CookieValue(name="email", required = false) String email, Model model, HttpServletResponse response) throws IOException {
         if (email != null) {
             User user = userService.findUserByEmail(email);
             if (user != null) {
+                // section for user info
                 model.addAttribute("user", user);
+                // section for cart quantity
+                List<CartItem> cartItems = cartService.getCartItems(userService.findUserByEmail(email));
+                int cartItemsCount = 0;
+                for (CartItem cartItem : cartItems) {
+                    cartItemsCount += cartItem.getQuantity();
+                }
+                model.addAttribute("cartItemsCount", cartItemsCount);
+                // section for avatar
                 if (user.getAvatar() != null) {
                     model.addAttribute("base64Avatar", Base64.getEncoder().encodeToString(user.getAvatar()));
                 }
