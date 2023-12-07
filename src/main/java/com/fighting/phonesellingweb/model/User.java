245583386fb155142095c5ce8504fd3b5760fa1e
ngotanloi0709 <52100439@student.tdtu.edu.vn>
2023-12-07 13:39:55 +0700

    P38 - Bugfix of Delete User

--- src/main/java/com/fighting/phonesellingweb/model/User.java
+++ src/main/java/com/fighting/phonesellingweb/model/User.java
@@ -34,8 +34,14 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductViewHistory> viewHistory;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Favorite> favorites;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
 
     @Lob
     @Column(name = "avatar", columnDefinition = "LONGBLOB")
